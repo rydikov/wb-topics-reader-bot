b86a02ad42a06da39b5e0b816d24cb62822db25f
Maxim Rydikov <rydikov@gmail.com>
2025-05-27 14:30:13 +0300

    flake8

--- bot.py
+++ bot.py
@@ -40,22 +40,25 @@ logger.info(mqtt_topics)
 dp = Dispatcher()
 dp.message.filter(F.from_user.id.in_(config['users'].keys()))
 
-### MQTT
+# MQTT
 topics_data = {}
 mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 WB_MQTT_HOST = os.environ['WB_MQTT_HOST']
 WB_MQTT_PORT = int(os.environ['WB_MQTT_PORT'])
 
+
 def on_connect(client, userdata, flags, reason_code, properties):
     logger.info("MQTT подключено")
     subscriptions = [(topic, 0) for topic in mqtt_topics]
     client.subscribe(subscriptions)
 
+
 def on_message(client, userdata, msg):
     payload = msg.payload.decode('utf-8')
     topics_data[msg.topic] = payload
     logger.info(f"MQTT: {msg.topic} => {payload}")
+
 
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
@@ -63,22 +66,24 @@ mqtt_client.connect(WB_MQTT_HOST, WB_MQTT_PORT, 60)
 mqtt_client.loop_start()
 ###
 
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
-### TG Commands
+
+# TG Commands
 @dp.message(Command('status'))
 async def command_status(message: Message) -> None:
 
     answer = ""
 
     for val in config['users'][message.from_user.id]:
-        print(val)
         answer += hcode(f"{val['label']}: {topics_data.get(val['topic'], 'Нет данных')} \n")
 
     await message.answer(answer)
 ###
+
 
 @dp.message()
 async def default_handler(message: types.Message) -> None:
